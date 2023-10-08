160a5ac7f13032da219399857591b6358e59a3b8
danielktaylor <daniel-k-taylor@users.noreply.github.com>
2023-10-08 14:36:20 -0700

    Super basic websockets nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+	ws.send('I got your: ' + data)
+  });
+
+  ws.send('something');
+});
