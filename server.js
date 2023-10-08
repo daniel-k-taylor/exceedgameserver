0678dd55ffee0d4293f8c5a2a8399c6d0e9923e9
danielktaylor <daniel-k-taylor@users.noreply.github.com>
2023-10-08 16:27:04 -0700

    Change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { WebSocketServer } from 'ws';
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: 443 });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
